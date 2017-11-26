8666375c1d01c321c1b788db8ff2a1d2ac529f32
ryan <ryan@zipcoders-MacBook-Pro-5.local>
2017-11-25 21:06:28 -0500

    not done, but good progress

--- src/main/java/io/zipcoder/ItemParser.java
+++ src/main/java/io/zipcoder/ItemParser.java
@@ -2,23 +2,62 @@ package io.zipcoder;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class ItemParser {
 
 
     public ArrayList<String> parseRawDataIntoStringArray(String rawData){
         String stringPattern = "##";
-        ArrayList<String> response = splitStringWithRegexPattern(stringPattern , rawData);
+        ArrayList<String> response = splitStringWithRegexPattern(stringPattern, rawData);
         return response;
     }
 
-    public Item parseStringIntoItem(String rawItem) throws ItemParseException{
-        return null;
+    public String parseString(String regex, String rawItem) throws ItemParseException{
+        Pattern namePattern = Pattern.compile(regex, Pattern.CASE_INSENSITIVE);
+        Matcher nameMatcher = namePattern.matcher(rawItem);
+        String name;
+        if (nameMatcher.find()) {
+            Integer startingIndex = nameMatcher.start();
+            Integer endingIndex = nameMatcher.end();
+            name = rawItem.substring(startingIndex, endingIndex);
+            //System.out.println(name);
+        } else {
+            throw new ItemParseException();
+        }
+        return name;
+
+    }
+
+    public Double parseDouble(String rawItem) throws ItemParseException{
+        Pattern pricePattern = Pattern.compile("\\d+[.]\\d+", Pattern.CASE_INSENSITIVE);
+        Matcher priceMatcher = pricePattern.matcher(rawItem);
+        Double price;
+        if (priceMatcher.find()) {
+            Integer startingIndex = priceMatcher.start();
+            Integer endingIndex = priceMatcher.end();
+            price = Double.parseDouble(rawItem.substring(startingIndex, endingIndex));
+            //System.out.println(price);
+        } else {
+            throw new ItemParseException();
+        }
+        return price;
+    }
+
+
+    public Item parseStringIntoItem(String rawItem) throws ItemParseException {
+        String name = parseString("(?<=name:)\\w+", rawItem);
+        Double price = parseDouble(rawItem);
+        String type = parseString("(?<=type:)\\w+",rawItem);
+        String expiration = parseString("\\d+[/]\\d+[/]\\d+",rawItem);
+
+        return new Item(name.toLowerCase(), price, type.toLowerCase(), expiration.toLowerCase());
     }
 
     public ArrayList<String> findKeyValuePairsInRawItemData(String rawItem){
-        String stringPattern = "[;|^]";
-        ArrayList<String> response = splitStringWithRegexPattern(stringPattern , rawItem);
+        String stringPattern = "[;@^*%]";
+        ArrayList<String> response = splitStringWithRegexPattern(stringPattern, rawItem);
         return response;
     }
 
--- src/main/java/io/zipcoder/Main.java
+++ src/main/java/io/zipcoder/Main.java
@@ -2,18 +2,72 @@ package io.zipcoder;
 
 import org.apache.commons.io.IOUtils;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
 
 public class Main {
 
-    public String readRawDataToString() throws Exception{
+    private static ArrayList<Item> itemsList = new ArrayList<Item>();
+    private static Integer numberOfErrors = 0;
+
+    public String readRawDataToString() throws Exception {
         ClassLoader classLoader = getClass().getClassLoader();
         String result = IOUtils.toString(classLoader.getResourceAsStream("RawData.txt"));
         return result;
     }
 
-    public static void main(String[] args) throws Exception{
+    public static void main(String[] args) throws Exception {
         String output = (new Main()).readRawDataToString();
-        System.out.println(output);
-        // TODO: parse the data in output into items, and display to console.
+        //System.out.println(output);
+        ItemParser itemParser = new ItemParser();
+        ArrayList<String> itemStrings = itemParser.parseRawDataIntoStringArray(output);
+        for (String itemString : itemStrings) {
+            //System.out.println(itemString);
+            try {
+                Item newItem = itemParser.parseStringIntoItem(itemString);
+                itemsList.add(newItem);
+                //System.out.println(newItem);
+            } catch (ItemParseException ipe) {
+                numberOfErrors++;
+            }
+        }
+        //Double used for Price, Integer used for Number of Times seen
+        HashMap<Double, Integer> milkPriceHashMap = new HashMap<>();
+        HashMap<Double, Integer> breadPriceHashMap = new HashMap<>();
+        HashMap<Double, Integer> cookiesPriceHashMap = new HashMap<>();
+        HashMap<Double, Integer> applesPriceHashMap = new HashMap<>();
+
+        setKeysAndValuesForHashMap("[Mm][Ii][Ll][Kk]", milkPriceHashMap);
+        setKeysAndValuesForHashMap("[Bb][Rr][Ee][Aa][Dd]", breadPriceHashMap);
+        setKeysAndValuesForHashMap("[cC][0oO]{2}[kK][iI][eE][sS]", cookiesPriceHashMap);
+        setKeysAndValuesForHashMap("[aA][pP]{2}[lL][eE][sS]", applesPriceHashMap);
+
+        System.out.println(numberOfErrors);
+
+    }
+    //sout just used to see what happens, would be taken out in the end. Prints the correct keys and values,
+    //just not the corresponding names for each.
+    private static void setKeysAndValuesForHashMap(String regex, HashMap<Double, Integer> map) {
+        for (Item item : itemsList) {
+            if (item.getName().matches(regex)) {
+                if (map.containsKey(item.getPrice())) {
+                    Integer value = map.get(item.getPrice());
+                    value = value + 1;
+                    map.put(item.getPrice(), value);
+                }
+                else {
+                    map.put(item.getPrice(), 1);
+                }
+            }
+        }
+        for (Map.Entry<Double, Integer> entry : map.entrySet()) {
+            System.out.println(entry.getKey() + " " + entry.getValue());
+        }
     }
 }
+
+
+
+
